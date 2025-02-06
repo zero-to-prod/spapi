a50ae375f3ae991c3958b1d3f55f798382d60119
david_smith <david_smith@sweetwater.com>
2025-02-06 10:47:37 -0500

    refactor/orders Updated `tests/Unit/Lwa/LwaTest.php`.

--- tests/Unit/Lwa/LwaTest.php
+++ tests/Unit/Lwa/LwaTest.php
@@ -27,24 +27,6 @@ class LwaTest extends TestCase
     private const TEST_SCOPE = 'test-scope';
 
     /** @test */
-    public function client_credentials(): void
-    {
-        $response = Lwa::from(
-            'client_id',
-            'client_secret',
-            'https://httpbin.org/post',
-            'user-agent'
-        )->refreshToken('refresh_token');
-
-        self::assertEquals(200, $response['info']['http_code']);
-        self::assertEquals('client_id', $response['response']['form']['client_id']);
-        self::assertEquals('client_secret', $response['response']['form']['client_secret']);
-        self::assertEquals('refresh_token', $response['response']['form']['grant_type']);
-        self::assertEquals('refresh_token', $response['response']['form']['refresh_token']);
-        self::assertEquals('user-agent', $response['response']['form']['user-agent']);
-    }
-
-    /** @test */
     public function it_creates_instance_with_minimal_parameters(): void
     {
         $lwa = new Lwa(self::TEST_CLIENT_ID, self::TEST_CLIENT_SECRET);
