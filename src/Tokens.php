7a157c6174654307ff5c13004fbd6d1292ddd5e4
david_smith <david_smith@sweetwater.com>
2025-02-06 13:56:29 -0500

    feat/order-items Updated `src/Tokens.php`.

--- src/Tokens.php
+++ src/Tokens.php
@@ -7,10 +7,6 @@ use Zerotoprod\SpapiTokens\SpapiTokens;
 class Tokens
 {
     /**
-     * @var array
-     */
-    private $dataElements;
-    /**
      * @var string|null
      */
     private $targetApplication;
@@ -34,14 +30,12 @@ class Tokens
 
     public function __construct(
         string $access_token,
-        array $dataElements = [],
         ?string $targetApplication = null,
         string $base_uri = 'https://sellingpartnerapi-na.amazon.com/tokens/2021-03-01/restrictedDataToken',
         ?string $user_agent = null,
         array $options = []
     ) {
         $this->access_token = $access_token;
-        $this->dataElements = $dataElements;
         $this->targetApplication = $targetApplication;
         $this->user_agent = $user_agent;
         $this->base_uri = $base_uri;
@@ -50,20 +44,20 @@ class Tokens
 
     public static function from(
         string $access_token,
-        array $dataElements = [],
         ?string $targetApplication = null,
         string $base_uri = 'https://sellingpartnerapi-na.amazon.com/tokens/2021-03-01/restrictedDataToken',
         ?string $user_agent = null,
         array $options = []
     ): self {
-        return new self($access_token, $dataElements, $targetApplication, $base_uri, $user_agent, $options);
+        return new self($access_token, $targetApplication, $base_uri, $user_agent, $options);
     }
 
     /**
      * Creates a Restricted Data Token.
      *
-     * @param  string  $order_id  Amazon Order ID
-     * @param  array   $options   Merge curl options
+     * @param  string  $order_id      Amazon Order ID
+     * @param  array   $dataElements  `['buyerInfo', 'shippingAddress']`
+     * @param  array   $options       Merge curl options
      *
      * @return array{
      *  info: array{
@@ -125,12 +119,12 @@ class Tokens
      *      restrictedDataToken: string
      *  }
      */
-    public function order(string $order_id, array $options = []): array
+    public function order(string $order_id, array $dataElements = [], array $options = []): array
     {
         return SpapiTokens::createRestrictedDataToken(
             $this->access_token,
             '/orders/v0/orders/'.$order_id,
-            $this->dataElements,
+            $dataElements,
             $this->targetApplication,
             $this->base_uri,
             $this->user_agent,
@@ -141,7 +135,9 @@ class Tokens
     /**
      * Creates a Restricted Data Token.
      *
-     * @param  array  $options  Merge curl options
+     * @param  string  $order_id      Amazon Order ID
+     * @param  array   $dataElements  `['buyerInfo']`
+     * @param  array   $options       Merge curl options
      *
      * @return array{
      *  info: array{
@@ -203,12 +199,91 @@ class Tokens
      *      restrictedDataToken: string
      *  }
      */
-    public function orders(array $options = []): array
+    public function orderItems(string $order_id, array $dataElements = [], array $options = []): array
+    {
+        return SpapiTokens::createRestrictedDataToken(
+            $this->access_token,
+            '/orders/v0/orders/'.$order_id.'/orderItems',
+            $dataElements,
+            $this->targetApplication,
+            $this->base_uri,
+            $this->user_agent,
+            array_merge($this->options, $options)
+        );
+    }
+
+    /**
+     * Creates a Restricted Data Token.
+     *
+     * @param  array  $dataElements  `['buyerInfo', 'shippingAddress']`
+     * @param  array  $options       Merge curl options
+     *
+     * @return array{
+     *  info: array{
+     *      url: string,
+     *      content_type: string,
+     *      http_code: int,
+     *      header_size: int,
+     *      request_size: int,
+     *      filetime: int,
+     *      ssl_verify_result: int,
+     *      redirect_count: int,
+     *      total_time: float,
+     *      namelookup_time: float,
+     *      connect_time: float,
+     *      pretransfer_time: float,
+     *      size_upload: int,
+     *      size_download: int,
+     *      speed_download: int,
+     *      speed_upload: int,
+     *      download_content_length: int,
+     *      upload_content_length: int,
+     *      starttransfer_time: float,
+     *      redirect_time: float,
+     *      redirect_url: string,
+     *      primary_ip: string,
+     *      certinfo: array,
+     *      primary_port: int,
+     *      local_ip: string,
+     *      local_port: int,
+     *      http_version: int,
+     *      protocol: int,
+     *      ssl_verifyresult: int,
+     *      scheme: string,
+     *      appconnect_time_us: int,
+     *      connect_time_us: int,
+     *      namelookup_time_us: int,
+     *      pretransfer_time_us: int,
+     *      redirect_time_us: int,
+     *      starttransfer_time_us: int,
+     *      total_time_us: int
+     *  },
+     *  error: string,
+     *  headers: array{
+     *      Server: string,
+     *      Date: string,
+     *      Content-Type: string,
+     *      Content-Length: string,
+     *      Connection: string,
+     *      'x-amz-rid': string,
+     *      'x-amzn-RateLimit-Limit': string,
+     *      'x-amzn-RequestId': string,
+     *      'x-amz-apigw-id': string,
+     *      'X-Amzn-Trace-Id': string,
+     *      Vary: string,
+     *      'Strict-Transport-Security': string
+     *  },
+     *  response: array{
+     *      expiresIn: int,
+     *      restrictedDataToken: string
+     *  }
+     */
+    public function orders(array $dataElements = [], array $options = []): array
     {
         return SpapiTokens::createRestrictedDataToken(
             $this->access_token,
             '/orders/v0/orders',
-            $this->dataElements,
+            $dataElements,
             $this->targetApplication,
             $this->base_uri,
             $this->user_agent,
