0291f193dbb1beba963141de950b83a235e6d284
david_smith <david_smith@sweetwater.com>
2025-02-06 13:56:29 -0500

    feat/order-items Updated `src/Orders.php`.

--- src/Orders.php
+++ src/Orders.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Zerotoprod\Spapi;
 
+use Zerotoprod\CurlHelper\CurlHelper;
 use Zerotoprod\SpapiOrders\SpapiOrders;
 
 /**
@@ -50,132 +51,6 @@ class Orders
         $this->base_uri = $base_uri;
         $this->user_agent = $user_agent;
         $this->options = $options;
-    }
-
-    /**
-     * Returns the order that you specify.
-     *
-     * @param  string  $orderId  Amazon Order Id
-     * @param  array   $options  Merge curl options
-     *
-     * @return array{
-     *     info: array{
-     *         url: string,
-     *         content_type: string,
-     *         http_code: int,
-     *         header_size: int,
-     *         request_size: int,
-     *         filetime: int,
-     *         ssl_verify_result: int,
-     *         redirect_count: int,
-     *         total_time: float,
-     *         namelookup_time: float,
-     *         connect_time: float,
-     *         pretransfer_time: float,
-     *         size_upload: int,
-     *         size_download: int,
-     *         speed_download: int,
-     *         speed_upload: int,
-     *         download_content_length: int,
-     *         upload_content_length: int,
-     *         starttransfer_time: float,
-     *         redirect_time: float,
-     *         redirect_url: string,
-     *         primary_ip: string,
-     *         certinfo: array,
-     *         primary_port: int,
-     *         local_ip: string,
-     *         local_port: int,
-     *         http_version: int,
-     *         protocol: int,
-     *         ssl_verifyresult: int,
-     *         scheme: string,
-     *         appconnect_time_us: int,
-     *         connect_time_us: int,
-     *         namelookup_time_us: int,
-     *         pretransfer_time_us: int,
-     *         redirect_time_us: int,
-     *         starttransfer_time_us: int,
-     *         total_time_us: int
-     *     },
-     *     error: string,
-     *     headers: array{
-     *         Server: string,
-     *         Date: string,
-     *         Content-Type: string,
-     *         Content-Length: string,
-     *         Connection: string,
-     *         X-Amz-Rid: string,
-     *         X-Amzn-Ratelimit-Limit: string,
-     *         X-Amzn-Requestid: string,
-     *         X-Amz-Apigw-Id: string,
-     *         X-Amzn-Trace-Id: string,
-     *         Vary: string,
-     *         Strict-Transport-Security: string
-     *     },
-     *     response: array{
-     *         payload: array{
-     *             BuyerInfo: array{
-     *                 BuyerEmail: string,
-     *                 BuyerName: string
-     *             },
-     *             AmazonOrderId: string,
-     *             EarliestShipDate: string,
-     *             SalesChannel: string,
-     *             OrderStatus: string,
-     *             NumberOfItemsShipped: int,
-     *             OrderType: string,
-     *             IsPremiumOrder: bool,
-     *             IsPrime: bool,
-     *             FulfillmentChannel: string,
-     *             NumberOfItemsUnshipped: int,
-     *             HasRegulatedItems: bool,
-     *             IsReplacementOrder: bool,
-     *             IsSoldByAB: bool,
-     *             LatestShipDate: string,
-     *             ShipServiceLevel: string,
-     *             IsISPU: bool,
-     *             MarketplaceId: string,
-     *             PurchaseDate: string,
-     *             ShippingAddress: array{
-     *                 StateOrRegion: string,
-     *                 AddressLine1: string,
-     *                 PostalCode: string,
-     *                 City: string,
-     *                 CountryCode: string,
-     *                 Name: string
-     *             },
-     *             IsAccessPointOrder: bool,
-     *             SellerOrderId: string,
-     *             PaymentMethod: string,
-     *             IsBusinessOrder: bool,
-     *             OrderTotal: array{
-     *                 CurrencyCode: string,
-     *                 Amount: string
-     *             },
-     *             PaymentMethodDetails: array<string>,
-     *             IsGlobalExpressEnabled: bool,
-     *             LastUpdateDate: string,
-     *             ShipmentServiceLevelCategory: string
-     *         },
-     *          errors: array{
-     *              code: string,
-     *              message: string,
-     *              details: string
-     *          }
-     *     }
-     * }
-     * @link https://developer-docs.amazon.com/sp-api/docs/orders-api-v0-reference#get-ordersv0ordersorderid
-     */
-    public function getOrder(string $orderId, array $options = []): array
-    {
-        return SpapiOrders::getOrder(
-            $this->base_uri,
-            $this->access_token,
-            $orderId,
-            $this->user_agent,
-            array_merge($this->options, $options)
-        );
     }
 
     /**
@@ -416,6 +291,269 @@ class Orders
             $EarliestDeliveryDateAfter,
             $LatestDeliveryDateBefore,
             $LatestDeliveryDateAfter,
+            $this->user_agent,
+            array_merge($this->options, $options)
+        );
+    }
+
+    /**
+     * Returns the order that you specify.
+     *
+     * @param  string  $orderId  Amazon Order Id
+     * @param  array   $options  Merge curl options
+     *
+     * @return array{
+     *     info: array{
+     *         url: string,
+     *         content_type: string,
+     *         http_code: int,
+     *         header_size: int,
+     *         request_size: int,
+     *         filetime: int,
+     *         ssl_verify_result: int,
+     *         redirect_count: int,
+     *         total_time: float,
+     *         namelookup_time: float,
+     *         connect_time: float,
+     *         pretransfer_time: float,
+     *         size_upload: int,
+     *         size_download: int,
+     *         speed_download: int,
+     *         speed_upload: int,
+     *         download_content_length: int,
+     *         upload_content_length: int,
+     *         starttransfer_time: float,
+     *         redirect_time: float,
+     *         redirect_url: string,
+     *         primary_ip: string,
+     *         certinfo: array,
+     *         primary_port: int,
+     *         local_ip: string,
+     *         local_port: int,
+     *         http_version: int,
+     *         protocol: int,
+     *         ssl_verifyresult: int,
+     *         scheme: string,
+     *         appconnect_time_us: int,
+     *         connect_time_us: int,
+     *         namelookup_time_us: int,
+     *         pretransfer_time_us: int,
+     *         redirect_time_us: int,
+     *         starttransfer_time_us: int,
+     *         total_time_us: int
+     *     },
+     *     error: string,
+     *     headers: array{
+     *         Server: string,
+     *         Date: string,
+     *         Content-Type: string,
+     *         Content-Length: string,
+     *         Connection: string,
+     *         X-Amz-Rid: string,
+     *         X-Amzn-Ratelimit-Limit: string,
+     *         X-Amzn-Requestid: string,
+     *         X-Amz-Apigw-Id: string,
+     *         X-Amzn-Trace-Id: string,
+     *         Vary: string,
+     *         Strict-Transport-Security: string
+     *     },
+     *     response: array{
+     *         payload: array{
+     *             BuyerInfo: array{
+     *                 BuyerEmail: string,
+     *                 BuyerName: string
+     *             },
+     *             AmazonOrderId: string,
+     *             EarliestShipDate: string,
+     *             SalesChannel: string,
+     *             OrderStatus: string,
+     *             NumberOfItemsShipped: int,
+     *             OrderType: string,
+     *             IsPremiumOrder: bool,
+     *             IsPrime: bool,
+     *             FulfillmentChannel: string,
+     *             NumberOfItemsUnshipped: int,
+     *             HasRegulatedItems: bool,
+     *             IsReplacementOrder: bool,
+     *             IsSoldByAB: bool,
+     *             LatestShipDate: string,
+     *             ShipServiceLevel: string,
+     *             IsISPU: bool,
+     *             MarketplaceId: string,
+     *             PurchaseDate: string,
+     *             ShippingAddress: array{
+     *                 StateOrRegion: string,
+     *                 AddressLine1: string,
+     *                 PostalCode: string,
+     *                 City: string,
+     *                 CountryCode: string,
+     *                 Name: string
+     *             },
+     *             IsAccessPointOrder: bool,
+     *             SellerOrderId: string,
+     *             PaymentMethod: string,
+     *             IsBusinessOrder: bool,
+     *             OrderTotal: array{
+     *                 CurrencyCode: string,
+     *                 Amount: string
+     *             },
+     *             PaymentMethodDetails: array<string>,
+     *             IsGlobalExpressEnabled: bool,
+     *             LastUpdateDate: string,
+     *             ShipmentServiceLevelCategory: string
+     *         },
+     *          errors: array{
+     *              code: string,
+     *              message: string,
+     *              details: string
+     *          }
+     *     }
+     * }
+     * @link https://developer-docs.amazon.com/sp-api/docs/orders-api-v0-reference#get-ordersv0ordersorderid
+     */
+    public function getOrder(string $orderId, array $options = []): array
+    {
+        return SpapiOrders::getOrder(
+            $this->base_uri,
+            $this->access_token,
+            $orderId,
+            $this->user_agent,
+            array_merge($this->options, $options)
+        );
+    }
+
+    /**
+     * Returns detailed order item information for the order that you specify. If NextToken is provided, it's used to retrieve the next page of order items.
+     *
+     * Note: When an order is in the Pending state (the order has been placed but payment has not been authorized), the getOrderItems operation does not return information about pricing, taxes, shipping charges, gift status or promotions for the order items in the order. After an order leaves the Pending state (this occurs when payment has been authorized) and enters the Unshipped, Partially Shipped, or Shipped state, the getOrderItems operation returns information about pricing, taxes, shipping charges, gift status and promotions for the order items in the order.
+     *
+     * @param  string  $orderId       Amazon order id
+     * @param  array   $options       Merge curl options.
+     *
+     * @return array{
+     *     info: array{
+     *         url: string,
+     *         content_type: string,
+     *         http_code: int,
+     *         header_size: int,
+     *         request_size: int,
+     *         filetime: int,
+     *         ssl_verify_result: int,
+     *         redirect_count: int,
+     *         total_time: float,
+     *         namelookup_time: float,
+     *         connect_time: float,
+     *         pretransfer_time: float,
+     *         size_upload: int,
+     *         size_download: int,
+     *         speed_download: int,
+     *         speed_upload: int,
+     *         download_content_length: int,
+     *         upload_content_length: int,
+     *         starttransfer_time: float,
+     *         redirect_time: float,
+     *         redirect_url: string,
+     *         primary_ip: string,
+     *         certinfo: array,
+     *         primary_port: int,
+     *         local_ip: string,
+     *         local_port: int,
+     *         http_version: int,
+     *         protocol: int,
+     *         ssl_verifyresult: int,
+     *         scheme: string,
+     *         appconnect_time_us: int,
+     *         connect_time_us: int,
+     *         namelookup_time_us: int,
+     *         pretransfer_time_us: int,
+     *         redirect_time_us: int,
+     *         starttransfer_time_us: int,
+     *         total_time_us: int
+     *     },
+     *     error: string,
+     *     headers: array{
+     *         Server: string,
+     *         Date: string,
+     *         Content-Type: string,
+     *         Content-Length: string,
+     *         Connection: string,
+     *         X-Amz-Rid: string,
+     *         X-Amzn-Ratelimit-Limit: string,
+     *         X-Amzn-Requestid: string,
+     *         X-Amz-Apigw-Id: string,
+     *         X-Amzn-Trace-Id: string,
+     *         Vary: string,
+     *         Strict-Transport-Security: string
+     *     },
+     *     response: array{
+     *          payload: array{
+     *              AmazonOrderId: string,
+     *              NextToken: string,
+     *              OrderItems: array<array{
+     *                  ASIN: string,
+     *                  OrderItemId: string,
+     *                  SellerSKU: string,
+     *                  Title: string,
+     *                  QuantityOrdered: int,
+     *                  QuantityShipped: int|null,
+     *                  PointsGranted: array{
+     *                      PointsNumber: int,
+     *                      PointsMonetaryValue: array{
+     *                          CurrencyCode: string,
+     *                          Amount: string
+     *                      }
+     *                  }|null,
+     *                  ItemPrice: array{
+     *                      CurrencyCode: string,
+     *                      Amount: string
+     *                  },
+     *                  ShippingPrice: array{
+     *                      CurrencyCode: string,
+     *                      Amount: string
+     *                  }|null,
+     *                  ScheduledDeliveryEndDate: string|null,
+     *                  ScheduledDeliveryStartDate: string|null,
+     *                  CODFee: array{
+     *                      CurrencyCode: string,
+     *                      Amount: string
+     *                  }|null,
+     *                  CODFeeDiscount: array{
+     *                      CurrencyCode: string,
+     *                      Amount: string
+     *                  }|null,
+     *                  PriceDesignation: string,
+     *                  BuyerInfo: array{
+     *                      BuyerCustomizedInfo: array{
+     *                          CustomizedURL: string
+     *                      },
+     *                      GiftMessageText: string,
+     *                      GiftWrapPrice: array{
+     *                          CurrencyCode: string,
+     *                          Amount: string
+     *                      },
+     *                      GiftWrapLevel: string
+     *                  }|null,
+     *                  BuyerRequestedCancel: array{
+     *                      IsBuyerRequestedCancel: string,
+     *                      BuyerCancelReason: string
+     *                  }|null,
+     *                  SerialNumbers: array<string>|null,
+     *                  PromotionIds: array<string>|null,
+     *                  ConditionId: string|null,
+     *                  ConditionSubtypeId: string|null,
+     *                  ConditionNote: string|null
+     *              }>
+     *          }
+     *      }
+     * }
+     * @link https://developer-docs.amazon.com/sp-api/docs/orders-api-v0-reference#get-ordersv0ordersorderid
+     */
+    public function getOrderItems(string $orderId, array $options = []): array
+    {
+        return SpapiOrders::getOrderItems(
+            $this->base_uri,
+            $this->access_token,
+            $orderId,
             $this->user_agent,
             array_merge($this->options, $options)
         );
